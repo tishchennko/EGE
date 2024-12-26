a77c523409b51ec91ad9f12c5be8166d983a82cb
macbook <boris.tichshenko2007@gmail.com>
2024-12-27 00:59:32 +0200

    Initial commit

--- Дз-27-12-24/task-8.py
+++ Дз-27-12-24/task-8.py
@@ -2,9 +2,9 @@ from itertools import product
 
 alph = sorted('ЯНВАРЬ')
 
-res = []
+ans = []
 for pos, val in enumerate(product(alph, repeat = 5), 1):
     val = ''.join(val)
     if val[0] != 'Я' and val.count('Ь') == 1 and 'ЯЯ' not in val:
-        res.append(pos)
-print(max(res))
+        ans.append(pos)
+print(max(ans))
