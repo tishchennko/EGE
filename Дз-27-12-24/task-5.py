94fe235776d905ed90e479b41f85964aac664f48
macbook <boris.tichshenko2007@gmail.com>
2024-12-26 20:49:25 +0200

    Initial commit

---
+++ Дз-27-12-24/task-5.py
@@ -0,0 +1,19 @@
+
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+
+ans = []
+for N in range(1, 100_000):
+    R = convert(N, 3)
+    if N % 3 == 0:
+        R = R + R[-2:]
+    else:
+        R  += convert(sum(map(int, R)), 3)
+    R = int(R, 3)
+    if R > 220 and R % 2 == 0:
+        ans.append(R)
+print(min(ans))
