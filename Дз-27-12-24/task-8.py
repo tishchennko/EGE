c3d02009df447cc215997a8e3e81d9fbf160133d
BTishenko <boris.tichshenko2007@gmail.com>
2024-12-27 15:42:22 +0200

    исправил 8 задание

--- Дз-27-12-24/task-8.py
+++ Дз-27-12-24/task-8.py
@@ -5,6 +5,6 @@ alph = sorted('ЯНВАРЬ')
 ans = []
 for pos, val in enumerate(product(alph, repeat = 5), 1):
     val = ''.join(val)
-    if val[0] != 'Я' and val.count('Ь') == 1 and 'ЯЯ' not in val:
-        ans.append(ans)
+    if 'Я' not in val[0] and val.count('Ь') == 1 and 'ЯЯ' not in val:
+        ans.append(pos)
 print(max(ans))
