4fbfc278245b7467fffec7b13080d40160ac50bb
BTishenko <boris.tichshenko2007@gmail.com>
2025-01-14 17:27:48 +0200

    w z y x

---
+++ 14-01-25-task-2/task-2-4582.py
@@ -0,0 +1,12 @@
+from itertools import product, permutations
+
+def f(x, y, w, z):
+    return (not (w <= z)) or (x <= y) or (not x)
+
+for a1, a2, a3, a4, a5, a6, a7 in product([1,0], repeat = 7):
+    table =[(1, a1, a2, a3), (0, 1, 0, a4), (a5, 0, a6, a7)]
+    if len(set(table))==len(table):
+        for p in permutations('xywz'):
+            u = [f(**(dict(zip(p,t)))) for t in table] == [0, 0, 0]
+            if u:
+                print(*p)
