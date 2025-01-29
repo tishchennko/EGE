e24c2945a6021b0f840d678d07fe97b9cf843a6d
macbook <boris.tichshenko2007@gmail.com>
2025-01-29 11:11:58 +0200

    commit

--- DZ-16-01-25/task-8.py
+++ DZ-16-01-25/task-8.py
@@ -1,13 +1,13 @@
-
+from string import*
 from itertools import*
 
+alph = digits + ascii_lowercase
 
 cnt = 0
-for val in product('0123456789', repeat = 5):
+for val in product(alph[:16], repeat = 5):
     if val.count('6') == 2 and val[0] != 0:
         x = ''.join(val)
-        x = x.replace('0', '8', 1).replace('2', '8', 1).replace('4', '8', 1) \
-            .replace('6', '8', 1)
+        x = x.replace('0', '8', 1).replace('2', '8', 1).replace('4', '8', 1)
         if x.count('86') == 0 and x.count('68') ==0:
             cnt += 1
 
