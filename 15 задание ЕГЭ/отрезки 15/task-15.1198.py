4649d9c5f5f2cecf631844ec3bf69288b72ad033
BTishenko <boris.tichshenko2007@gmail.com>
2025-02-07 17:00:22 +0200

    36

---
+++ 15 задание ЕГЭ/отрезки 15/task-15.1198.py
@@ -0,0 +1,15 @@
+from itertools import *
+def f(x):
+    B = 18 <= x <= 52
+    C = 16 <= x <= 41
+    A = A1 <= x <= A2
+    return (B <= A) and ((not C) or A)
+
+ans = []
+line = [x / 10 for x in range(15 * 10, 55 * 10)]
+
+for A1,A2 in combinations(line, 2):
+    if all(f(x) for x in line):
+        ans.append(A2-A1)
+
+print(min(ans))
