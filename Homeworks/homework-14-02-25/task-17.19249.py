dd6646190e5a97ac97a07888b6b17694ba908249
macbook <boris.tichshenko2007@gmail.com>
2025-02-12 14:30:15 +0200

    решил первое 17 из дз

---
+++ Homeworks/homework-14-02-25/task-17.19249.py
@@ -0,0 +1,19 @@
+with open('17_19249.txt') as file:
+    data = [int(i) for i in file]
+
+ans = []
+max_43 = max(i for i in data if len(str(abs(i))) == 5 and str(abs((i)))[-2:] == '43')
+for i in range(len(data) - 2):
+    num1, num2, num3 = data[i:i+3]
+    summ = (num1**2) + (num2**2) + (num3**2)
+    summ1 = num1 + num2 + num3
+    u1 = 10000 <= abs(num1) <= 99999 and str(abs(num1))[-2:] == '43'
+    u2 = 10000 <= abs(num2) <= 99999 and str(abs(num2))[-2:] == '43'
+    u3 = 10000 <= abs(num3) <= 99999 and str(abs(num3))[-2:] == '43'
+
+    f1 = u1 + u2 + u3 >= 1
+    f2 = summ <= (max_43) ** 2
+    if f1 and f2:
+        ans.append(summ)
+print(len(ans), min(ans))
+#92 838850571
