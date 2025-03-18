27517f09ee0c87789b7db28ccd3292fb15beaeb0
macbook <boris.tichshenko2007@gmail.com>
2025-03-18 16:50:42 +0200

    5 задание

---
+++ Пробники ЕГЭ/probnik-18-03-25/task-5.14328.py
@@ -0,0 +1,19 @@
+def f(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+
+ans = []
+for N in range(1, 10000):
+    R = f(N, 12)
+    if N % 3 == 0:
+        R = '1' + R + 'B'
+    else:
+        R = '2' + R + '0'
+    R = int(R, 12)
+    if R < 1996:
+        ans.append(R)
+print(max(ans))
+#1991
