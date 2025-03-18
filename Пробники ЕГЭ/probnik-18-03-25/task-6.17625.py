ba012954d03b6d514dd9d664cc92be457a87f67f
macbook <boris.tichshenko2007@gmail.com>
2025-03-18 17:02:31 +0200

    6 задание

--- Пробники ЕГЭ/probnik-18-03-25/task-6.17625.py
+++ Пробники ЕГЭ/probnik-18-03-25/task-6.17625.py
@@ -4,4 +4,28 @@ m = 10
 tracer(0)
 lt(90)
 
-for
+for i in range(10):
+    fd(22 * m)
+    rt(90)
+    fd(16 * m)
+    rt(90)
+up()
+fd(1 * m)
+rt(90)
+fd(1 * m)
+lt(90)
+down()
+for i in range(10):
+    fd(72 * m)
+    rt(90)
+    fd(79 * m)
+    rt(90)
+
+up()
+for x in range(-50, 50):
+    for y in range(-50, 50):
+        goto(x * m, y * m)
+        dot(5, 'white')
+
+done()
+#(21 + 15) * 2 == 72
