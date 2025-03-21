3f39132648870ed18c863789ed6c20883f96017c
macbook <boris.tichshenko2007@gmail.com>
2025-03-21 16:11:01 +0200

    28

---
+++ 5 задание ЕГЭ/11-03-25/task-5.20182.py
@@ -0,0 +1,20 @@
+```
+def f(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+
+ans = []
+for n in range(1, 100_000):
+    R = f(n, 3)
+    if sum(map(int, R)) % 2 == 0:
+        R = '12' + R[2:] + '0'
+    else:
+        R = '10' + R[2:] + '2'
+    R = int(R, 3)
+    if R > 105:
+        ans.append(n)
+print(min(ans))
+#28
